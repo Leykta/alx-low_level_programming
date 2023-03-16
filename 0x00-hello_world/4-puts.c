688f90875c149fdfa9976168d0af02742e7fa9f2
@Leykta <leykunta@gmail.com>
2023-03-16 05:42:28 -0700

    Task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * 
- * /
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
