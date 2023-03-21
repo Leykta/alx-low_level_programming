e32cf8c5235b734c8a86e60994de6f1553fe14af
@Leykta <leykunta@gmail.com>
2023-03-20 23:33:54 -0700

    C Functions project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * prints the alphabet in lowercase
+ *
+ * return 0 on success
+ */
+
+void print_alphabe(void)
+{
+        char ch;
+
+	ch='a';
+
+	while(ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
