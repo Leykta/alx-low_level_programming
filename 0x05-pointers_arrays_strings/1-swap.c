dc60dd85789ebc321a6e804778f1c3e785f10e71
@Leykta <leykunta@gmail.com>
2023-03-27 09:27:57 -0700

    Task 0 and 1 commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @*a - a parameter
+ * @*b - a parameter
+ *
+ * Description: swaps the value  of two integers
+ * returns nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
