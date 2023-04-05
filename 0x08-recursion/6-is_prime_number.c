bde0087af251728a28700467a21a6c6f8e565411
@Leykta <leykunta@gmail.com>
2023-04-04 23:53:48 -0700

    Last task updated

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
 
-int prime(unsigned int n, unsigned int c)
+int _prime(unsigned int n, unsigned int c)
 {
 	if (n % c == 0)
 	{
@@ -33,5 +33,5 @@ int is_prime_number(int n)
 		return (0);
 	if (n == 1)
 		return (0);
-	return (prime(n, 2));
+	return (_prime(n, 2));
 }
