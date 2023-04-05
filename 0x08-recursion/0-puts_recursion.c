23e2c7ec72bcbd456950718d3d37e43cd50b9252
@Leykta <leykunta@gmail.com>
2023-04-04 23:33:28 -0700

    Recursion project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 0-puts_recuresion - prints a string followed by new line
+ * @s - string parameter
+ *
+ * Return - nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
